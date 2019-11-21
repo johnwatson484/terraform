cf07f6d83cb7ced5b69c148f8b25daffe96a29af
John Watson <john.watson2@rpa.gov.uk>
2019-11-21 11:32:27 +0000

    EKS main.tf updated for cluster 001

--- aws/eks/main.tf
+++ aws/eks/main.tf
@@ -1,329 +1,76 @@
-# ACCOUNT
+# TERRAFORM CONFIG
+
+terraform {
+  backend "s3" {}
+}
+
 provider "aws" {
-  profile    = "johnwatsonaws1"
-  region     = "eu-west-2"
+  version = ">= 1.2"
+  region = "${var.region}"
 }
 
-# CLUSTER VARIABLES
-variable "cluster-name" {
-  default = "terraform-eks-demo"
-  type    = string
-}
+# ACOUNT ID
+data "aws_caller_identity" "current" {}
 
-# SUBNET
-# This data source is included for ease of sample architecture deployment
-# and can be swapped out as necessary.
-data "aws_availability_zones" "available" {
-}
-
-resource "aws_vpc" "demo" {
-  cidr_block = "10.0.0.0/16"
-
-  tags = {
-    "Name"                                      = "terraform-eks-demo-node"
-    "kubernetes.io/cluster/${var.cluster-name}" = "shared"
+## SECURITY GROUP STATE
+data "terraform_remote_state" "sgp_state" {
+  backend = "s3"
+  config {
+    bucket = "${data.aws_caller_identity.current.account_id}-${var.region}-${lower(var.service["code"])}-terraform-shared-state"
+    key = "/${var.region}/${var.environment}/security_groups/terraform.tfstate"
+    region = "${var.region}"
   }
 }
 
-resource "aws_subnet" "demo" {
-  count = 2
-
-  availability_zone = data.aws_availability_zones.available.names[count.index]
-  cidr_block        = "10.0.${count.index}.0/24"
-  vpc_id            = aws_vpc.demo.id
-
-  tags = {
-    "Name"                                      = "terraform-eks-demo-node"
-    "kubernetes.io/cluster/${var.cluster-name}" = "shared"
+## SUBNET STATE
+data "terraform_remote_state" "sbn_state" {
+  backend = "s3"
+  config {
+    bucket = "${data.aws_caller_identity.current.account_id}-${var.region}-${lower(var.service["code"])}-terraform-shared-state"
+    key = "/${var.region}/${var.environment}/subnets/terraform.tfstate"
+    region = "${var.region}"
   }
 }
 
-}
-»
-# IAM POLICY
-resource "aws_iam_role" "demo-node" {
-  name = "terraform-eks-demo-cluster"
-
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "eks.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-POLICY
-}
-
-resource "aws_iam_role_policy_attachment" "demo-cluster-AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = "${aws_iam_role.demo-node.name}"
-}
-
-resource "aws_iam_role_policy_attachment" "demo-cluster-AmazonEKSServicePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
-  role       = "${aws_iam_role.demo-node.name}"
-}
-
-# MASTER SECURITY GROUP
-resource "aws_security_group" "demo-cluster" {
-  name        = "terraform-eks-demo-cluster"
-  description = "Cluster communication with worker nodes"
-  vpc_id      = "${aws_vpc.demo.id}"
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "terraform-eks-demo"
+# IAM GROUP STATE
+data "terraform_remote_state" "iam_state" {
+  backend = "s3"
+  config {
+    bucket = "${data.aws_caller_identity.current.account_id}-${var.region}-${lower(var.service["code"])}-terraform-shared-state"
+    key    = "/${var.region}/${var.environment}/iam/terraform.tfstate"
+    region = "${var.region}"
   }
 }
 
-# OPTIONAL: Allow inbound traffic from your local workstation external IP
-#           to the Kubernetes. You will need to replace A.B.C.D below with
-#           your real IP. Services like icanhazip.com can help you find this.
-resource "aws_security_group_rule" "demo-cluster-ingress-workstation-https" {
-  cidr_blocks       = ["A.B.C.D/32"]
-  description       = "Allow workstation to communicate with the cluster API Server"
-  from_port         = 443
-  protocol          = "tcp"
-  security_group_id = "${aws_security_group.demo-cluster.id}"
-  to_port           = 443
-  type              = "ingress"
-}
-
-# MASTER CLUSTER
-resource "aws_eks_cluster" "demo" {
-  name            = "${var.cluster-name}"
-  role_arn        = "${aws_iam_role.demo-node.arn}"
-
+# EKS CLUSTER
+resource "aws_eks_cluster" "eks_cluster_001" {
+  depends_on = ["aws_cloudwatch_log_group.eks_cluster_log_group_001"]
+  name = "${var.eks_cluster_001}"
+  role_arn = "${data.terraform_remote_state.iam_state.eks_cluster_role_arn}"
+  enabled_cluster_log_types = ["api", "audit"]
+  version = "${var.eks_version}"
   vpc_config {
-    security_group_ids = ["${aws_security_group.demo-cluster.id}"]
-    subnet_ids         = ["${aws_subnet.demo.*.id}"]
-  }
-
-  depends_on = [
-    "aws_iam_role_policy_attachment.demo-cluster-AmazonEKSClusterPolicy",
-    "aws_iam_role_policy_attachment.demo-cluster-AmazonEKSServicePolicy",
-  ]
+    security_group_ids = ["${data.terraform_remote_state.sgp_state.eks_cluster_sgp001}"]
+    subnet_ids = ["${data.terraform_remote_state.sbn_state.int_a_sbn_1_id}", "${data.terraform_remote_state.sbn_state.int_b_sbn_1_id}", "${data.terraform_remote_state.sbn_state.int_c_sbn_1_id}"]
+    endpoint_private_access = "true"
+    endpoint_public_access = "false"
+  }  
 }
 
-# KUBECTL
-locals {
-  kubeconfig = <<KUBECONFIG
-
-
-apiVersion: v1
-clusters:
-- cluster:
-    server: ${aws_eks_cluster.demo.endpoint}
-    certificate-authority-data: ${aws_eks_cluster.demo.certificate_authority.0.data}
-  name: kubernetes
-contexts:
-- context:
-    cluster: kubernetes
-    user: aws
-  name: aws
-current-context: aws
-kind: Config
-preferences: {}
-users:
-- name: aws
-  user:
-    exec:
-      apiVersion: client.authentication.k8s.io/v1alpha1
-      command: aws-iam-authenticator
-      args:
-        - "token"
-        - "-i"
-        - "${var.cluster-name}"
-KUBECONFIG
-}
-
-output "kubeconfig" {
-  value = "${local.kubeconfig}"
-}
-
-# WORKER NODE IAM
-resource "aws_iam_role" "demo-cluster" {
-  name = "terraform-eks-demo-cluster"
-
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "eks.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-POLICY
-
-}
-
-resource "aws_iam_role_policy_attachment" "demo-cluster-AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.demo-node.name
-}
-
-resource "aws_iam_role_policy_attachment" "demo-cluster-AmazonEKSServicePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
-  role       = aws_iam_role.demo-node.name
-}
-
-# WORKER NODE SECURITY GROUP
-resource "aws_security_group" "demo-node" {
-  name        = "terraform-eks-demo-node"
-  description = "Security group for all nodes in the cluster"
-  vpc_id      = aws_vpc.demo.id
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    "Name"                                      = "terraform-eks-demo-node"
-    "kubernetes.io/cluster/${var.cluster-name}" = "owned"
+# CLOUDWATCH LOG GROUP
+resource "aws_cloudwatch_log_group" "eks_cluster_log_group_001" {
+  name = "${var.eks_cluster_name_001}-logs"
+  retention_in_days = 1
+  tags {
+    Name = "${var.eks_cluster_name_001}-logs"
+    Environment = "${var.env}"
+    Project = "${var.service["code"]}"
+    Region = "${var.region}"
+    Tier = "APP"
+    ServiceCode = "${var.service["code"]}"
+    ServiceType = "${var.service["type"]}"
+    ServiceName = "${var.service["name"]}"
+    CreationDateTime = "${timestamp()}"
+    Source = "Terraform Managed"
   }
 }
-
-resource "aws_security_group_rule" "demo-node-ingress-self" {
-  description              = "Allow node to communicate with each other"
-  from_port                = 0
-  protocol                 = "-1"
-  security_group_id        = aws_security_group.demo-node.id
-  source_security_group_id = aws_security_group.demo-node.id
-  to_port                  = 65535
-  type                     = "ingress"
-}
-
-resource "aws_security_group_rule" "demo-node-ingress-cluster" {
-  description              = "Allow worker Kubelets and pods to receive communication from the cluster control      plane"
-  from_port                = 1025
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.demo-node.id
-  source_security_group_id = aws_security_group.demo-cluster.id
-  to_port                  = 65535
-  type                     = "ingress"
- }
-»
-
-# CLUSTER NETWORKING
-resource "aws_security_group_rule" "demo-cluster-ingress-node-https" {
-  description              = "Allow pods to communicate with the cluster API Server"
-  from_port                = 443
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.demo-cluster.id
-  source_security_group_id = aws_security_group.demo-node.id
-  to_port                  = 443
-  type                     = "ingress"
-}
-
-# WORKER NODE AUTOSCALING GROUP
-data "aws_ami" "eks-worker" {
-  filter {
-    name   = "name"
-    values = ["amazon-eks-node-${aws_eks_cluster.demo.version}-v*"]
-  }
-
-  most_recent = true
-  owners      = ["602401143452"] # Amazon EKS AMI Account ID
-}
-
-# AUTOSCALING LAUNCH CONFIGURATION
-# This data source is included for ease of sample architecture deployment
-# and can be swapped out as necessary.
-data "aws_region" "current" {
-}
-
-# EKS currently documents this required userdata for EKS worker nodes to
-# properly configure Kubernetes applications on the EC2 instance.
-# We implement a Terraform local here to simplify Base64 encoding this
-# information into the AutoScaling Launch Configuration.
-# More information: https://docs.aws.amazon.com/eks/latest/userguide/launch-workers.html
-locals {
-  demo-node-userdata = <<USERDATA
-#!/bin/bash
-set -o xtrace
-/etc/eks/bootstrap.sh --apiserver-endpoint '${aws_eks_cluster.demo.endpoint}' --b64-cluster-ca '${aws_eks_cluster. demo.certificate_authority[0].data}' '${var.cluster-name}'
-USERDATA
-
-}
-
-resource "aws_launch_configuration" "demo" {
-  associate_public_ip_address = true
-  iam_instance_profile        = aws_iam_instance_profile.demo-node.name
-  image_id                    = data.aws_ami.eks-worker.id
-  instance_type               = "t2.micro"
-  name_prefix                 = "terraform-eks-demo"
-  security_groups  = [aws_security_group.demo-node.id]
-  user_data_base64 = base64encode(local.demo-node-userdata)
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-# RUN AUTOSCALING CONFIGURATION
-resource "aws_autoscaling_group" "demo" {
-  desired_capacity     = 2
-  launch_configuration = aws_launch_configuration.demo.id
-  max_size             = 2
-  min_size             = 1
-  name                 = "terraform-eks-demo"
-  vpc_zone_identifier = [aws_subnet.demo.*.id]
-
-  tag {
-    key                 = "Name"
-    value               = "terraform-eks-demo"
-    propagate_at_launch = true
-  }
-
-  tag {
-    key                 = "kubernetes.io/cluster/${var.cluster-name}"
-    value               = "owned"
-    propagate_at_launch = true
-  }
-}
-
-# CONFIG MAP TO ALLOW WORKER NODES TO JOIN CLUSTER
-locals {
-  config_map_aws_auth = <<CONFIGMAPAWSAUTH
-
-
-apiVersion: v1
-kind: ConfigMap
-metadata:
-  name: aws-auth
-  namespace: kube-system
-data:
-  mapRoles: |
-    - rolearn: ${aws_iam_role.demo-node.arn}
-      username: system:node:{{EC2PrivateDNSName}}
-      groups:
-        - system:bootstrappers
-        - system:nodes
-CONFIGMAPAWSAUTH
-
-}
-
-output "config_map_aws_auth" {
-  value = local.config_map_aws_auth
-}
-
